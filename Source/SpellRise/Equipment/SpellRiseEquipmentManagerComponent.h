c8e668fd02d0a25903c020aa9d9529447ef0501f
Edilson biM <edilsonbim@hotmail.com.br>
2026-04-06 02:05:54 -0300

    Weapon quick-slots: sync preview, hotkeys and inventory cleanup

--- Source/SpellRise/Equipment/SpellRiseEquipmentManagerComponent.h
+++ Source/SpellRise/Equipment/SpellRiseEquipmentManagerComponent.h
@@ -2,15 +2,22 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "GameplayAbilitySpec.h"
 #include "Net/Serialization/FastArraySerializer.h"
 #include "SpellRiseEquipmentManagerComponent.generated.h"
 
 class UEquippableItem;
 class UEquipmentComponent;
 class USpellRiseEquipmentInstance;
+class AActor;
+class USkeletalMeshComponent;
+class UNarrativeInventoryComponent;
+class UNarrativeItem;
 class UActorChannel;
 class FOutBunch;
 struct FReplicationFlags;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnQuickWeaponSlotsChanged);
 
 USTRUCT()
 struct FSpellRiseAppliedEquipmentEntry : public FFastArraySerializerItem
@@ -82,6 +89,29 @@ public:
 	virtual bool ReplicateSubobjects(UActorChannel* Channel, FOutBunch* Bunch, FReplicationFlags* RepFlags) override;
 	virtual void ReadyForReplication() override;
 
+	/** Client-safe entrypoint. Routes to server when called without authority. */
+	UFUNCTION(BlueprintCallable, Category="SpellRise|Equipment")
+	bool RequestEquipItem(UEquippableItem* Item);
+
+	/** Client-safe entrypoint. Routes to server when called without authority. */
+	UFUNCTION(BlueprintCallable, Category="SpellRise|Equipment")
+	bool RequestUnequipItem(UEquippableItem* Item);
+
+	UFUNCTION(BlueprintCallable, Category="SpellRise|Equipment")
+	bool RequestActivateQuickWeaponSlot(int32 QuickSlotIndex);
+
+	UFUNCTION(BlueprintCallable, Category="SpellRise|Equipment")
+	bool RequestAssignQuickWeaponSlot(UEquippableItem* Item, int32 QuickSlotIndex);
+
+	UFUNCTION(BlueprintPure, Category="SpellRise|Equipment")
+	UEquippableItem* GetQuickWeaponItemByIndex(int32 QuickSlotIndex) const;
+
+	UFUNCTION(BlueprintPure, Category="SpellRise|Equipment")
+	int32 GetActiveQuickWeaponSlotIndex() const { return ActiveQuickWeaponSlotIndex; }
+
+	UPROPERTY(BlueprintAssignable, Category="SpellRise|Equipment")
+	FOnQuickWeaponSlotsChanged OnQuickWeaponSlotsChanged;
+
 	UFUNCTION(BlueprintCallable, Category="SpellRise|Equipment")
 	bool EquipItem(UEquippableItem* Item);
 
@@ -96,10 +126,49 @@ public:
 	void HandleEntryRemoved(const FSpellRiseAppliedEquipmentEntry& Entry);
 
 private:
+	UFUNCTION(Server, Reliable)
+	void ServerRequestEquipItem(UEquippableItem* Item);
+
+	UFUNCTION(Server, Reliable)
+	void ServerRequestUnequipItem(UEquippableItem* Item);
+
+	UFUNCTION(Server, Reliable)
+	void ServerRequestActivateQuickWeaponSlot(int32 QuickSlotIndex);
+
+	UFUNCTION(Server, Reliable)
+	void ServerRequestAssignQuickWeaponSlot(UEquippableItem* Item, int32 QuickSlotIndex);
+
 	bool ValidateItemOwnership(UEquippableItem* Item, FString& OutReason) const;
 	uint8 ResolveItemSlot(UEquippableItem* Item) const;
 	UEquipmentComponent* ResolveEquipmentComponent() const;
 	void ApplyVisualForItem(UEquippableItem* Item, bool bEquip);
+	bool ExtractAbilitiesToGrantFromItem(UEquippableItem* Item, TArray<struct FSpellRiseGrantedAbility>& OutAbilities) const;
+	bool ResolveWeaponActorClassFromItem(UEquippableItem* Item, UClass*& OutWeaponActorClass, const void*& OutWeaponConfigPtr, const UStruct*& OutWeaponConfigStruct) const;
+	bool ResolveWeaponSocketsFromConfig(const void* WeaponConfigPtr, const UStruct* WeaponConfigStruct, FName& OutEquippedSocket, FName& OutStowedSocket) const;
+	AActor* ResolveOwnedWeaponActor(UClass* WeaponActorClass) const;
+	USkeletalMeshComponent* ResolveEquipmentAttachMesh() const;
+	void SnapItemWeaponToSocket(UEquippableItem* Item, bool bEquip);
+	void ApplyGrantedAbilitiesForSlot(UEquippableItem* Item, uint8 SlotValue);
+	void RemoveGrantedAbilitiesForSlot(uint8 SlotValue);
+	bool IsWeaponItem(UEquippableItem* Item) const;
+	int32 FindQuickSlotByItem(UEquippableItem* Item) const;
+	int32 FindFirstFreeQuickSlot() const;
+	void HandleWeaponEquipIntent(UEquippableItem* Item);
+	bool ActivateQuickWeaponSlot_Server(int32 QuickSlotIndex);
+	bool AssignQuickWeaponSlot_Server(UEquippableItem* Item, int32 QuickSlotIndex);
+	void RemoveQuickWeaponSlot_Server(int32 QuickSlotIndex, bool bDestroyWeaponActor);
+	AActor* GetOrSpawnWeaponActorForItem(UEquippableItem* Item);
+	void DestroyWeaponActorForItem(UEquippableItem* Item);
+	void RefreshQuickSlotVisual_Server(int32 QuickSlotIndex, bool bEquipped);
+	void SetNarrativeItemActiveState(UEquippableItem* Item, bool bShouldBeActive);
+	void CleanupOrphanedWeaponActors_Server();
+	void BindInventoryRemovalHook();
+	UFUNCTION()
+	void HandleInventoryItemRemoved(UNarrativeItem* RemovedItem, int32 Amount);
+	UFUNCTION()
+	void OnRep_QuickWeaponSlots();
+	UFUNCTION()
+	void OnRep_ActiveQuickSlotIndex();
 
 	FSpellRiseAppliedEquipmentEntry* AddEntry(UEquippableItem* Item, uint8 SlotValue);
 	bool RemoveEntryBySlot(uint8 SlotValue);
@@ -107,4 +176,16 @@ private:
 private:
 	UPROPERTY(Replicated)
 	FSpellRiseEquipmentList EquipmentList;
+
+	TMap<uint8, TArray<FGameplayAbilitySpecHandle>> GrantedAbilityHandlesBySlot;
+	TMap<TObjectPtr<UEquippableItem>, TObjectPtr<AActor>> SpawnedWeaponActorsByItem;
+
+	UPROPERTY(ReplicatedUsing=OnRep_QuickWeaponSlots)
+	TArray<TObjectPtr<UEquippableItem>> QuickWeaponSlots;
+
+	UPROPERTY(ReplicatedUsing=OnRep_ActiveQuickSlotIndex)
+	int32 ActiveQuickWeaponSlotIndex = INDEX_NONE;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UNarrativeInventoryComponent> CachedInventoryComponent = nullptr;
 };
